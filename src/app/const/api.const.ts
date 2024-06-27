a7f8f8f47047a792fa05b72c03bb61485a7f36d0
Mike Stotik <mikestotik@gmail.com>
2024-06-27 13:59:44 +0300

    Added login

--- src/app/const/api.const.ts
+++ src/app/const/api.const.ts
@@ -1,6 +1,5 @@
 export namespace CoreApiUri {
-  export const AUTH_SIGN_IN = '/auth/sign-in';
-  export const AUTH_SIGN_UP = '/auth/sign-up';
+  export const AUTH_SIGN_IN = '/auth/login';
   export const AUTH_SIGN_OUT = '/auth/sign-out';
   export const AUTH_REFRESH = '/auth/refresh';
 
