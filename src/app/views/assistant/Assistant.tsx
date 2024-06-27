dd970270baed6fa61fef4482cfc7ec65200643ae
Mike Stotik <mikestotik@gmail.com>
2024-06-27 22:09:19 +0300

    Added AnimatedRing

--- src/app/views/assistant/Assistant.tsx
+++ src/app/views/assistant/Assistant.tsx
@@ -11,15 +11,7 @@ export const Assistant = observer(() => {
       </div>
 
       <div className="assistant-animation">
-        {/*<Scene*/ }
-        {/*  layers={ 5 }*/ }
-        {/*  speeds={ [ [ 5, 10 ], 20, 30, [ 35, 40 ], 50 ] }*/ }
-        {/*  colors={ [ [ '#74a7f1', '#ffffff00' ], '#ffffff', [ '#74a7f150', '#ffffff' ], [ '#74a7f1', '#ffffff50' ], [ '#74a7f150', '#ffffff' ] ] }*/ }
-        {/*  lineWidths={ [ [ 20, 40 ], 30, [ 40, 80 ], 70, 80 ] }*/ }
-        {/*  innerRadii={ [ [ 0.1, 0.8 ], 0.1, [ 0.1, 0.85 ], 0.1, 0.9 ] }*/ }
-        {/*  outerRadii={ [ [ 1, 1.1 ], 1.1, [ 1, 1.2 ], 1.1, 1.2 ] }*/ }
-        {/*  gradientWidths={ [ [ 0.05, 0.1 ], 0.1, [ 0.05, 0.15 ], 0.1, 0.2 ] }*/ }
-        {/*/>*/ }
+        ...
       </div>
 
       <div className="assistant-user">
